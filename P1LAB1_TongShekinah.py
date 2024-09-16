f3528e107c5d7ab253c27d96c98d708bf5e3e575
bewise-services <tongs6984@student.faytechcc.edu>
2024-09-16 11:24:15 -0400

    First upload

---
+++ P1LAB1_TongShekinah.py
@@ -0,0 +1,14 @@
+# Shekinah Tong
+# 09/16/2024
+# P1LAB1
+# Get input from user and display output
+
+# Get first name from the user
+first_name = input ("enter your first name: ")
+
+
+# Get last name from the user
+last_name = input ("enter your last name: ")
+
+# Display info to user
+print("Hello,", first_name, last_name +"!", "Welcome to CTI - 110")
